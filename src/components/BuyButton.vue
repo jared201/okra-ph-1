cf9f7b293c2e7dbd4f6a629d53e8f5716632bf37
Jared Odulio <jared@Jareds-MacBook-Pro.local>
2021-01-27 19:34:23 +0800

    added new components

---
+++ src/components/BuyButton.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container">
+        <div class="columns">
+            <div class="column">
+                <button class="button is-large is-fullwidth is-warning is-dark">BUY OKRA NOW</button>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'BuyButton'
+}
+</script>
---
+++ src/components/MainBody.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container">
+        <div class="columns">
+            
+            <div class="column">
+                <img src="https://i.ibb.co/qJRdG4R/IMG-1729.jpg" alt="IMG-1729" border="0">
+            </div>
+            <div class="column">
+                <img src="https://i.ibb.co/tL89gcd/IMG-1664.jpg" alt="IMG-1664" border="0">
+            </div>
+            
+        </div>        
+    </div>
+</template>
+<script>
+export default {
+    name: 'Body'
+}
+</script>
